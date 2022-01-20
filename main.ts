6ba1834c02583ba969761e02925d4c851f5a389b
Marcel Thomas <marcelthomas5@gmail.com>
2022-01-20 18:03:01 +0000

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,15 @@
-import { Application } from 'https://deno.land/x/oak/mod.ts';
+import { Nubo } from 'https://storage.nubo.codes/nubo/0.0.1/mod.ts';
 
-const PORT = parseInt(Deno.env.get('PORT') || '') || 8080;
-const app = new Application();
-const VERSION = 11;
+const VERSION = '0.0.1';
 
-app.use(async (ctx, next) => {
-  await next();
-  const rt = ctx.response.headers.get('X-Response-Time');
-  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+Nubo.router.get('/', ({ response }) => {
+  response.body = {
+    name: 'deno-test',
+    version: VERSION,
+    cloudProvider: Deno.env.get('NUBO_CLOUD_PROVIDER') || 'unknown',
+    region: Deno.env.get('NUBO_REGION') || 'unknown',
+    location: Deno.env.get('NUBO_LOCATION') || 'unknown',
+  };
 });
 
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.response.headers.set('X-Response-Time', `${ms}ms`);
-});
-
-app.use(async (ctx) => {
-  ctx.response.body = `app:${PORT} - v${VERSION}`;
-});
-
-console.log(`http://localhost:${PORT}`);
-console.log(`version ${VERSION}`);
-await app.listen({ port: PORT });
+Nubo.start();
