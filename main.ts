25545e5df45889e36e031e39afac5e2ce47d2593
Marcel Thomas <marcelthomas5@gmail.com>
2022-01-21 12:00:10 +0000

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import { Nubo } from 'https://storage.nubo.codes/@nubo/nubo/0.0.3/mod.ts';
-import { Slack } from 'https://storage.nubo.codes/@businessql/slack/0.0.1/mod.ts';
+import { Slack } from 'https://storage.nubo.codes/@businessql/slack/0.0.2/mod.ts';
+import { ConvertKit } from 'https://storage.nubo.codes/@businessql/convertkit/0.0.1/mod.ts';
 
 Nubo.router.get('/', ({ response }) => {
   response.body = {
@@ -18,6 +19,12 @@ Nubo.router.get('/send-message', async ({ response }) => {
   });
 
   response.body = result;
+});
+
+Nubo.router.get('/subscriber', async ({ response }) => {
+  const subscriber = await ConvertKit.getSubscriber({ id: '1594016775' });
+
+  response.body = { subscriber };
 });
 
 Nubo.start();
